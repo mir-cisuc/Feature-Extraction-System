5d38741058d953cc1be7415ae72a640fdec3161b
mcarolina5 <47608640+mcarolina5@users.noreply.github.com>
2020-11-01 22:17:14 +0000

    Titulo csv

--- src/StructuralFeatures/countTitle.java
+++ src/StructuralFeatures/countTitle.java
@@ -1,5 +1,6 @@
 package StructuralFeatures;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,6 +13,34 @@ public class countTitle {
 	public countTitle() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public static void writeCSV(String titulo, int value) {
+		String outputFolder  = "src/Output/";
+		FileWriter fileWriter2 = null;
+		try {
+			fileWriter2 = new FileWriter(outputFolder+"Titles.csv");
+			
+			fileWriter2.write("title, count");
+			fileWriter2.write("\n");
+			fileWriter2.write(titulo);
+			fileWriter2.write(", ");
+			fileWriter2.write(value);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (fileWriter2 != null) {
+					fileWriter2.flush();
+					fileWriter2.close();					
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -20,46 +49,15 @@ public class countTitle {
 		//String content = "we are the lovesick girls";
 		String titulo = "wish you were here".toLowerCase();	
 				
-		//content = content.replace(" "," ");
-		//content = content.replace("?"," ");
-		//content = content.replace(","," ");
-		//content = content.replace("!"," "); 
-		//content = content.replace("&"," ");
-		//content = content.replaceAll("\\r\\n|\\r|\\n", " ");
-
+		
+		int occurences=0;
+		occurences=content.split(titulo, -1).length-1;
 		System.out.println(content.split(titulo, -1).length-1);
+		writeCSV(titulo,occurences);
 		
-		/*String [] list = content.split(" ");
 		
-		int contador = 0;
-
-		for (int j = 0; j < list.length; j++) {
-			System.out.println(list[j]);
-			if (list[j].toLowerCase().equals(lista_titulo[0].toLowerCase())) {
-				if (check(lista_titulo,list,j+1)) {
-					//System.out.println(lista_titulo[0].toLowerCase() + " " + list[j].toLowerCase());
-					contador++;
-				}
-			}
-		}
-		System.out.println(contador);*/
 	}
 
-	/*public static boolean check (String [] titulo, String [] conteudo, int indice) {
-		for (int i = 1; i< titulo.length; i++) {
-			if (!titulo[i].toLowerCase().equals(conteudo[indice].toLowerCase())) {
-				return false;
-			}
-			indice++;
-		}
-		//System.out.println(titulo[i].toLowerCase() + " " + conteudo[indice].toLowerCase());
-		return true;
-	}
 	
-	public static void printList(String [] lista) {
-		for(String lista1 : lista) {
-			System.out.println(lista1);
-		}
-	}*/
 	
 }
