e50109dc6d9ec817fe0d96ebf0afcdd6a68ff1a2
hugoredinho <hugoredinho@gmail.com>
2021-02-24 16:58:58 +0000

    teste

--- extra_scripts/get_audio_from_youtube.py
+++ extra_scripts/get_audio_from_youtube.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 import youtube_dl
 from youtubesearchpython import VideosSearch
 import json
+<<<<<<< HEAD
+
+=======
 import pandas as pd
 from os import listdir
 from os.path import isfile, join
@@ -12,6 +15,7 @@ path_audio =  r'C:\Users\Red\Desktop\Investigacao2020\FeatureExtractionSystem\ex
 
 
 lista_urls = list()
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
 
 class MyLogger(object):
     def debug(self, msg):
@@ -34,19 +38,30 @@ ydl_opts = {
     'format': 'bestaudio/best',
     'logger': MyLogger(),
     'progress_hooks': [my_hook],
+<<<<<<< HEAD
+=======
     'outtmpl': path_audio + r'\%(title)s.%(ext)s',
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
 }
 
 def download_video(url):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+<<<<<<< HEAD
+        print('Starting the downloads')
+=======
         print('Starting the download of %s' % (url))
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
         ydl.download([url])
 
 
 #download_video('https://www.youtube.com/watch?v=p6OoY6xneI0')
 
+<<<<<<< HEAD
+def get_video(nome):
+=======
 def get_audio(nome):
     print("Searching for %s" % (nome))
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
     videosSearch = VideosSearch(nome, limit = 5)
 
     resultado_pesquisa = videosSearch.result()
@@ -67,24 +82,59 @@ def get_audio(nome):
 
     for resultado in lista_resultados:
        lista = list()
+<<<<<<< HEAD
+       for key,value in resultado.items():
+           # queremos extrair a duracao, as views
+=======
        print(resultado['title'])
        for key,value in resultado.items():
            # queremos extrair a duracao, as views
            
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
            if key == 'duration':
                tuplo = (key,value)
                lista.append(tuplo)
            if key == 'viewCount':
+<<<<<<< HEAD
+               tuplo = ('views',int(value['text'].split(' ')[0].replace(",","")))
+=======
                if value['text'] == 'No views':
                    tuplo = ('views',0)
                else:
                    tuplo = ('views',int(value['text'].split(' ')[0].replace(",","")))
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
                lista.append(tuplo)
            if key == 'link':
                tuplo = ('url',value)
                lista.append(tuplo) 
        lista_informacao.append(lista)
 
+<<<<<<< HEAD
+    
+           #print(key,value)
+    media_duracao, lista_final = create_duration_average(lista_informacao)
+
+    sorted_by_views = sorted(lista_informacao, key=sec_elem, reverse= True)
+
+
+    media_minutos = media_duracao[0]
+    media_segundos = media_duracao[1]
+
+    media_em_segundos = media_minutos * 60 + media_segundos
+
+    print(media_em_segundos)
+
+    for i in range(len(sorted_by_views)):
+        # if the duration is similar to the average, we pick that
+        link = sorted_by_views[i]
+        duracao = link[0][1]
+        split = duracao.split(":")
+        total = int(split[0]) * 60 +  int(split[1])
+
+        if (total * 1.2 > media_em_segundos) or (total * 0.8 < media_em_segundos):
+            print("0 %s funciona!!! tempos %s %s" %(i,media_duracao, (split[0],split[1])))
+            break
+=======
     if len(lista_informacao) != 0:
 
 
@@ -120,6 +170,7 @@ def get_audio(nome):
     else:
         with open("lista_urls.txt","a") as f_w:
             f_w.write(nome + " - " + "URL MA\n")
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
 
 
 def sec_elem(s):
@@ -135,7 +186,11 @@ def create_duration_average(lista):
                 numero = (int(split[0]),int(split[1]))
                 lista_final.append(numero)
     
+<<<<<<< HEAD
+    print(lista_final)
+=======
     #print(lista_final)
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
 
     tlist1 = list()
 
@@ -152,13 +207,24 @@ def create_duration_average(lista):
     seconds, centis = divmod(centis, 100)
     minutes, secs = divmod(seconds, 60)
 
+<<<<<<< HEAD
+    tuplo = (minutes,seconds)
+=======
     tuplo = (minutes,secs)
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
 
   
     print("Average time = %02d:%02d" % (minutes, secs))
 
     return tuplo, lista_final 
 
+<<<<<<< HEAD
+
+
+    
+
+get_video('why not loona audio')
+=======
 '''def get_names():
     files_lirica = [path_musicas + "\\" + f for f in listdir(path_musicas) if isfile(join(path_musicas, f))]
     files_audio = [path_audio + "\\" + f for f in listdir(path_audio) if isfile(join(path_audio, f))]
@@ -178,3 +244,4 @@ with open("urls.txt","w") as f_w:
 
             
 #get_names()
+>>>>>>> 2cda597d1e06c970ef6f4f5b40089f878e267e09
